5ce58852f2c036ea3d0b15cd808defe16eb5e7d6
Anand <Anandkumbhalkar34@gmail.com>
2024-07-21 17:13:50 +0530

    first commit

---
+++ src/test/java/testing1/kitelogin2.java
@@ -0,0 +1,18 @@
+package testing1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class kitelogin2 {
+	
+  @Test
+  public void f() {
+	  
+	  WebDriver driver= new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://www.chess.com/home");
+	  
+  }
+}
+
